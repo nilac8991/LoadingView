8aed091fcb2e40c8544e0546dc034b3bcb70d89d
Calin <nilac8991@gmail.com>
2019-02-13 10:39:50 +0100

    LoadingView: Allow the dialog to be fully immersive
    Signed-off-by: Calin <nilac8991@gmail.com>

--- loadingview/src/main/java/com/github/loadingview/LoadingDialog.kt
+++ loadingview/src/main/java/com/github/loadingview/LoadingDialog.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.graphics.drawable.ColorDrawable
 import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
+import android.view.WindowManager
 
 class LoadingDialog private constructor(internal var activity: Activity) {
 
@@ -17,11 +18,13 @@ class LoadingDialog private constructor(internal var activity: Activity) {
             try {
                 alertDialog!!.window!!.setBackgroundDrawable(ColorDrawable(0))
                 alertDialog!!.window!!.attributes.windowAnimations = R.style.DialogAnimations
+                alertDialog!!.window!!.setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
             } catch (e: Exception) {
 
             }
-
             alertDialog!!.show()
+            alertDialog!!.window!!.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility
+            alertDialog!!.window!!.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE)
             if (loadingView != null)
                 loadingView!!.start()
         }
